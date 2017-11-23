f52649dbf0747265a882c1e1fb9db3f473c057e9
James-Wetton <james.wetton@users.noreply.github.com>
2017-11-23 18:24:36 +1100

    added taxation and adding funds

--- Olabria_crypto.py
+++ Olabria_crypto.py
@@ -2,7 +2,7 @@ import time
 import os
 import sys
 import random
-
+taxamount = 49
 true = 1
 while true == 1:
     try:
@@ -15,9 +15,13 @@ while true == 1:
             usersavings = str(f.readline())
             print('\n \n')
             print(usersavings + " oad savings in account")
+            if '-' in usersavings:
+                print("*** YOU ARE " + usersavings + " OAD IN DEBT ***")
             print('\n')
             f.close()
             func = input("press 1 to exchange, press 2 to enter tax mode, press 3 to add funds ")
+
+
             if func == ('1'):
                 amount = input("How much would you like to exchange? ")
                 userx = input("Please type the citizen id of the person to exchange with: ")
@@ -39,15 +43,39 @@ while true == 1:
 
 
             if func == ('2'):
-                print("function doesnt exist ")
+                tax = input("This months tax is " + str(taxamount) + " oad. Ready to pay? ")
+                if tax in ('Yes', 'Y', 'y', 'yes'):
+                    if int(usersavings) > 50:
+                        f = open(id + '.txt', 'w')
+                        taxing = (int(usersavings) - int(taxamount))
+                        f.write(str(taxing))
+                        f.close()
+                        f = open('fedgov.txt', 'r')
+                        govamount = f.read()
+                        f.close()
+                        f = open('fedgov.txt', 'w')
+                        govtaxation = int(govamount) + int(taxamount)
+                        f.write(str(govtaxation))
+                        f.close()
+                        print("Completed the transaction.")
+                    else:
+                        print("Not enough savings in account. ")
+                else:
+                    print("Ok. Returning... ")
 
             if func == ('3'):
                 addfund = input("Please type in admin code: ")
-               
-
+                if addfund == ('Olabriastate'):
+                    print("making transaction...")
+                    f = open(id + '.txt', 'w')
+                    addedfunds = int(usersavings) + 100
+                    f.write(str(addedfunds))
+                    f.close()
+                    print("transaction complete!")
+                    print("You now have " + str(int(usersavings) + 100)  + " oad of savings in your account! ")
+                else:
+                    print("invalid code. Returning...")
         else:
             print("no existing id")
-
-
     except KeyboardInterrupt:
         break
